c4065fd7827107e16051e07003a5e966a51d400f
priyashah13 <priyashah2020@u.northwestern.edu>
2019-01-24 09:29:52 -0600

    asset and ask functions

--- student_code.py
+++ student_code.py
@@ -20,10 +20,16 @@ class KnowledgeBase(object):
     def kb_assert(self, fact):
         """Assert a fact or rule into the KB
 
+
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
+
+        check if fact is an instance of Fact - if it is, then append fact
         """
         print("Asserting {!r}".format(fact))
+        if(isinstance(fact, Fact)):
+            self.facts.append(fact)
+
         
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
@@ -33,5 +39,24 @@ class KnowledgeBase(object):
 
         Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
+
+        get all the bindings using the listofbindings class
+        for every fact in knowledge base, match
+        if match is not false, add match to bindings list
+
+        if the list of bindings is empty, return false
+        else, return the list of bindings
+
         """
         print("Asking {!r}".format(fact))
+        bindings = ListOfBindings()
+        if(isinstance(fact, Fact)):
+            for x in self.facts:
+                m = match(fact.statement, x.statement)
+                if (m != False):
+                    bindings.add_bindings(m)
+        if len(bindings) == 0:
+            return False
+        else:
+            return bindings
+
